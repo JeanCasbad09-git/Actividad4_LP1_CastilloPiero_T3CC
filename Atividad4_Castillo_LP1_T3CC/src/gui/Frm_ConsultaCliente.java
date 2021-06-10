767e40f879c63366df9585e116c8966327b219f6
JeanCasbad09-git <jeancasbad09@gmail.com>
2021-06-10 12:28:45 -0500

    Actividad4

---
+++ Atividad4_Castillo_LP1_T3CC/src/gui/Frm_ConsultaCliente.java
@@ -0,0 +1,42 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Frm_ConsultaCliente extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frm_ConsultaCliente frame = new Frm_ConsultaCliente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Frm_ConsultaCliente() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
---
+++ Atividad4_Castillo_LP1_T3CC/src/reporte/GenerarReporte.java
@@ -0,0 +1,5 @@
+package reporte;
+
+public class GenerarReporte {
+
+}
